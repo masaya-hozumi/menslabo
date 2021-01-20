00be17c26aef5de3b692f4e768784aa8c4bb6141
masaya-hozumi <masaya28.meel@gmail.com>
2021-01-20 20:02:23 +0900

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,13 +7,29 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   include ApplicationHelper
   # Run tests in parallel with specified workers
-  # parallelize(workers: :number_of_processors)　←並行テスト
+  # parallelize(workers: :number_of_processors) ←並行テスト
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  # テストユーザーがログイン中の場合にtrueを返す
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 
   # Add more helper methods to be used by all tests here...
